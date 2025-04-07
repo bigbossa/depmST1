59fb241f7be6108d08c43a9b167e67aed7ea789e
JTP.Codeing <110913626+bigbossa@users.noreply.github.com>
2025-04-07 19:26:13 +0700

    Add files via upload

--- public/login.php
+++ public/login.php
@@ -101,7 +101,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") { // Login handling
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel=" stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/stylelogin.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
     <title>Login Page</title>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
@@ -125,7 +125,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") { // Login handling
         <div class="form-container sign-in">
             <form method="POST" action="">
                 <div class="text-start w-100">
-                    <a href="../index.php" class="btn btn-primary text-white fw-bold">
+                    <a href="../index.php" class="btn btn-outline-secondary fw-bold "style="margin-top: -55px; margin-left: -15px;">
                         <i class="bi bi-arrow-left">Back</i>
                     </a>
                 </div>
@@ -155,7 +155,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") { // Login handling
         </div>
     </div>
 
-    <script src="../assets/js/script.js"></script>
+    <script src="../assets/js/scriptlogin.js"></script>
 
     <?php if (isset($_SESSION['swal_error'])): ?>
     <script>
